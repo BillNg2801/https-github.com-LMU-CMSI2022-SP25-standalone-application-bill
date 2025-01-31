cf6b8d2e9de99a80b61cb61e0c86016b2a35951b
asdf <asdf>
2025-01-30 16:21:33 -0800

    Initial Commit

---
+++ bill standalone app/bill_standalone_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bill_standalone_appApp.swift
+//  bill standalone app
+//
+//  Created by keckuser on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct bill_standalone_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bill standalone appTests/bill_standalone_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  bill_standalone_appTests.swift
+//  bill standalone appTests
+//
+//  Created by keckuser on 1/30/25.
+//
+
+import Testing
+@testable import bill_standalone_app
+
+struct bill_standalone_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
